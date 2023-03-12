2f25d2e04b5efaa7bd940709c3fa3e2d578c0b63
arisGio <apjoti@gmail.com>
2023-03-13 00:01:00 +0200

    Added filtering functionality.

--- index.js
+++ index.js
@@ -7,11 +7,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // EVENT LISTENERS
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // FUNCTIONS
 
@@ -61,4 +63,32 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    // console.log(todos);
+    todos.forEach( function(todo) {
+        if (todo.nodeType == Node.ELEMENT_NODE) {
+            switch (e.target.value) {
+                case "all":
+                    todo.style.display = 'flex';
+                    break;
+                case "completed":
+                    if (todo.classList.contains("completed")){
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "uncompleted":
+                    if(!todo.classList.contains('completed')){
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+            }
+    }
+    });
 }
