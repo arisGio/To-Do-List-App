d64ce1ac21aff90236f5aa3d89e82599439bf0ea
arisGio <apjoti@gmail.com>
2023-03-12 21:15:46 +0200

    Added description of the script's coding structure.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // create an folder for all the javascript that you are going to use
 // create a Task class in order to properly create & collect all the properties & methods that characterize a task
 // handle events when user uses the Interface
+
+// SELECTORS
+
+// EVENT LISTENERS
+
+// FUNCTIONS
